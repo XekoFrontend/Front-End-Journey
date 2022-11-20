2b7d257abd10007d12e491e55e81e22fe47e850b
PianoGuitar <104209111+PianoGuitar@users.noreply.github.com>
2022-11-20 18:00:21 +0700

    rolling dice - done

--- 3. JavaScript/07-Pig-Game/0.Practice/script.js
+++ 3. JavaScript/07-Pig-Game/0.Practice/script.js
@@ -1,1 +1,26 @@
-'use strict';
+"use strict";
+
+const score0El = document.querySelector("#score--0"); //El means Element
+const score1El = document.getElementById("score--1");
+
+const diceEl = document.querySelector(".dice");
+
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+
+diceEl.classList.add("hidden");
+
+//Rolling dice functionality
+btnRoll.addEventListener("click", function () {
+  //1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  //2. Display dice
+  diceEl.classList.remove("hidden");
+  diceEl.src = `img/dice-${dice}.png`;
+
+  //3. Check for rolled 1. If true, switch to next player
+});
